11e7390a5c8644eada592d5f48709f69d9dafea5
Shivani <shivanipatil270@gmail.com>
2022-01-20 21:02:33 +0530

    Created comming soon page

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="nav">
+    <!-- <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> -->
+  </div>
+  <router-view/>
+</template>
+
+<style>
+body{
+  margin: 0px;
+  padding: 0px;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  
+}
+
+#nav {
+  padding: 0px;
+  margin: 0px;
+  height: 0;
+}
+
+#nav a {
+  
+  
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,166 @@
+<template>
+  <div class="home">
+    <header class="header">
+        <div class="heade_div">
+          
+          <img type src="../assets/logo.png" alt="logo">
+          <h1 class="line1">LEANAFY</h1>
+          <h3 class="">Let's Create the Lean Way!</h3>
+        </div>
+
+      
+    </header>
+    <div class="content">
+      
+      
+      <h2 class="cont-1">COMING SOON</h2>
+
+    <section>
+       <div class="hello">
+   
+         <div class="days">{{displayDays}} :
+         <label for="days">days</label>
+         </div>
+         <div class="days">{{displayHours}} :
+          <label for="days">hours</label>
+          </div>
+         <div class="days">{{displayMinutes}} :
+            <label for="days">minutes</label>
+          </div>    
+         <div class="days">{{displaySeconds}} 
+          <label for="days">seconds</label>
+          </div>
+      </div>
+    </section>
+
+
+    </div>
+    
+   
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    displayDays: 0,
+    displayHours: 0,
+    displayMinutes: 0,
+    displaySeconds: 0
+  }),
+  computed: {
+    _seconds: () => 1000,
+    _minutes(){
+      return this._seconds * 60;
+    },
+    _hours(){
+      return this._minutes * 60;
+    },
+    _days(){
+      return this._hours * 24;
+    }
+  },
+  mounted() {
+    this.showRemaining()
+  },
+  methods: {
+    showRemaining(){
+      const timer = setInterval(() =>{
+        const now = new Date();
+        const end = new Date(2022,0,22,10,10,10,10);
+        const distance = end.getTime() - now.getTime();
+
+        if (distance < 0){
+          clearInterval(timer);
+          return
+        }
+
+        const days = Math.floor((distance/this._days));
+        const hours = Math.floor((distance% this._days) / this._hours);
+        const minutes = Math.floor((distance % this._days) / this._minutes);
+        const seconds = Math.floor((distance % this._days) / this._seconds);
+        this.displayMinutes = minutes < 10 ? '0' + minutes : minutes;
+        this.displaySeconds = seconds < 10 ? '0' + seconds : seconds;
+        this.displayHours = hours < 10 ? '0' + hours : hours;
+        this.displayDays = days < 10 ? '0' + days :days;
+      }, 1000);
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+.home{
+  display: grid;
+  grid-row-start: 1;
+  grid-row-end: 1.5;
+  background-image: linear-gradient(rgba(0, 0, 0, 0.5),rgba(6, 53, 46, 0.5)), url('../assets/warehouse.jpg');
+  height: 100vh;
+  width: 100vw;
+  
+}
+ .header{
+  color: white;
+  font-weight: bold;
+  font-size: 30px;
+  align-content: center;
+  line-height: 30px;
+  /* height: 20vh; */
+} 
+.content{
+  font-family: 'Dongle', sans-serif;
+  color: rgb(180, 199, 216);
+  font-weight: bold;
+  font-size: 30px;
+}
+img{
+  max-height: 70px;
+  max-width: 70px;
+  /* background-color: white; */
+  display: inline-block;
+  margin-top: 50px;
+  background-color: black;
+  
+}
+.heade_div{
+  color: rgb(198, 228, 228);
+  font-size: 18px;
+  font-family: 'Nunito', sans-serif;
+  letter-spacing: 4px;
+  
+} 
+/* .counter{
+  display: flex; 
+  -ms-flex-flow: row;
+  -ms-flex-item-align: center;
+  align-items: center; 
+} */
+.days{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: 25px;
+  font-variant-numeric: oldstyle-nums;
+  /* letter-spacing: 4px; */
+}
+.hello{
+  display: flex;
+  -ms-flex-item-align: center;
+  justify-content: center;
+  /* margin-right: auto; */
+  padding: 40px;
+}
+label{
+  font-size: 12px;
+  /* letter-spacing: 0; */
+}
+.cont-1{
+  font-family: 'Dongle', sans-serif;
+  letter-spacing: 40px;
+  font-weight: bold;
+  color: azure;
+}
+
+
+</style>
